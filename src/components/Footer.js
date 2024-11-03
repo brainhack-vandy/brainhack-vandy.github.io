6391666dcfb568ef1f0b7a86bd3112c66dde486a
shansitads <shansitads@gmail.com>
2024-11-03 08:16:07 -0600

    Added social media links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,65 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillYoutube,
+} from "react-icons/ai";
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
+          {/* additional footer information */}
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>BrainHack Vanderbilt</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/brainhack-vandy"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://x.com/BrainhackVU"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaXTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.youtube.com/@BrainHackVanderbilt"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillYoutube />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/brainhackvu/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
         </Col>
       </Row>
     </Container>
