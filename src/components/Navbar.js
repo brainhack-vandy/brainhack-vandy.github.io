8284cfe70c3a7262692742ba1ad93687999c2065
shansitads <shansitads@gmail.com>
2024-11-11 23:57:41 -0600

    rough schedule

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -76,13 +76,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-
-            <Nav.Item>
-              <Nav.Link as={Link} to="/schedule" onClick={() => updateExpanded(false)}>
-                <AiOutlineQuestionCircle className="nav-icon" /> Schedule
-              </Nav.Link>
-            </Nav.Item>
-
           </Nav>
         </Navbar.Collapse>
       </Container>
