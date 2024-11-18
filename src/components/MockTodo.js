c8c6c083595d6899fd15471787092e5cf288c45a
rgbayrak <rozgns@gmail.com>
2024-11-18 12:39:56 -0600

    added fork/star button

--- src/components/MockTodo.js
+++ src/components/MockTodo.js
@@ -1,18 +1,18 @@
-import issues from '../data/issues.json';
+// import issues from '../data/issues.json';
 
 
-export default function MockTodo() {
-    return (
-        <div>
-            <h1>Mock Todo</h1>
-            <ul>
-                {issues.map((issue, index) => (
-                    <li key={index}>
-                        <h3>{issue.title}</h3>
-                        <p>{issue.body}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+// export default function MockTodo() {
+//     return (
+//         <div>
+//             <h1>Mock Todo</h1>
+//             <ul>
+//                 {issues.map((issue, index) => (
+//                     <li key={index}>
+//                         <h3>{issue.title}</h3>
+//                         <p>{issue.body}</p>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
