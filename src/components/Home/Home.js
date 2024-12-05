19efe8253a63b5bb284448070d5163ae554009f1
shansitads <shansitads@gmail.com>
2024-12-05 00:06:30 -0600

    fixed spacing and parent spacing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,10 @@ import homeImg from "../../images/landing.svg";
 import Particle from "../Particle";
 import Introduction from "./Introduction";
 import Location from "./Location";
-// import Sponsors from "./Sponsors";
 import ProjectPitch from "./ProjectPitch";
 import Type from "./Type";
 
-function Home() {
+const Home = () => {
   return (
     <section>
       <Container fluid className="home-section" id="home">
@@ -16,34 +15,40 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 className="heading heading-bottom-margin">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+              <div className="content-wrapper">
+                <h1 className="heading">
+                  Hi There!{" "}
+                  <span className="wave" role="img" aria-labelledby="wave">
+                    👋🏻
+                  </span>
+                </h1>
 
-              <h1
-              >
+                <div className="title-wrapper">
+                  <h1 className="heading-name">
+                    Welcome to
+                    <strong className="main-name"> BrainHack Vanderbilt</strong>
+                  </h1>
+                </div>
 
-              </h1>
+                <div className="type-wrapper">
+                  <Type />
+                </div>
 
-              <h1 className="heading-name heading-small-margin">
-                Welcome to
-                <strong className="main-name"> BrainHack Vanderbilt</strong>
-              </h1>
-              <div style={{ padding: 50, textAlign: "left" }}>
-              <Type />
-              </div>
-              <p className="event-date">
-                January 24-26, 2025
-              </p>
-              <div className="button-container">
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSeX6AfQ0noO5hzIEO-SgBIcfsHfCuQyyEgLGxrP3N5UCg8w0w/viewform?usp=sharing" target="_blank" rel="noopener noreferrer">
-          <button className="btn btn-primary application-button">
-            Apply Here
-          </button>
-        </a>
+                <p className="event-date">
+                  January 24-26, 2025
+                </p>
+
+                <div className="button-wrapper">
+                  <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSeX6AfQ0noO5hzIEO-SgBIcfsHfCuQyyEgLGxrP3N5UCg8w0w/viewform"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button className="btn btn-primary application-button">
+                      Apply Here
+                    </button>
+                  </a>
+                </div>
               </div>
             </Col>
 
@@ -61,9 +66,8 @@ function Home() {
       <Introduction />
       <ProjectPitch />
       <Location />
-      {/* <Sponsors /> */}
     </section>
   );
-}
+};
 
 export default Home;
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,34 +1,31 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 
-function Type() {
+const Type = () => {
   return (
-    <div style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
-    <h1 style={{ margin: "0 10px 0 0", padding: "0", fontSize: "1.5em" }}>Open to</h1>
-      
-      <Typewriter
-        options={{
-          strings: [
-            "Researchers",
-            "Clinicians",
-            "Surgeons",
-            "Students",
-            "Data Scientists",
-            "Software Developers",
-            "Faculty",
-            "Postdocs",
-          ],
-          autoStart: true,
-          loop: true,
-          deleteSpeed: 50,
-          
-          
-          
-        }}
-      />
-      
+    <div className="type-container">
+      <span className="type-fixed-text">Open to</span>
+      <div className="type-writer-wrapper">
+        <Typewriter
+          options={{
+            strings: [
+              "Researchers",
+              "Clinicians",
+              "Surgeons",
+              "Students",
+              "Data Scientists",
+              "Software Developers",
+              "Faculty",
+              "Postdocs",
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default Type;
