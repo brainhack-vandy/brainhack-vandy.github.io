818f9cc51f906e68bbcb49a36f920ee61b257615
shansitads <shansitads@gmail.com>
2024-12-20 14:36:14 -0600

    widened landing and footer at bottom for ultra wide screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,57 +5,53 @@ import {
   AiFillInstagram,
   AiFillYoutube,
 } from "react-icons/ai";
-import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright @ 2025 BrainHack Vanderbilt</h3>
-        </Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/brainhack-vandy"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.youtube.com/@BrainHackVanderbilt"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillYoutube />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/brainhackvu/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-          </ul>
-        </Col>
-        <Col md="4" className="footer-recognitions">
-          <span>
-            <h3>Design based on{" "}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col md={4} className="footer-copywright">
+            <h3>Copyright @ 2025 BrainHack Vanderbilt</h3>
+          </Col>
+          <Col md={4} className="footer-body">
+            <ul className="footer-icons">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/brainhack-vandy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.youtube.com/@BrainHackVanderbilt"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiFillYoutube />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/brainhackvu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={4} className="footer-recognitions">
+            <h3>
+              Design based on{" "}
               <a
                 href="https://github.com/soumyajit4419/Portfolio"
                 target="_blank"
                 rel="noopener noreferrer"
-                style={{ color: "white", fontSize: "10px"}}
               >
                 soumyajit4419
               </a>{" "}
@@ -64,7 +60,6 @@ function Footer() {
                 href="https://brainhackwestern.github.io/"
                 target="_blank"
                 rel="noopener noreferrer"
-                style={{ color: "white", fontSize: "10px"}}
               >
                 Brainhack Western
               </a>
@@ -73,15 +68,15 @@ function Footer() {
                 href="https://storyset.com/"
                 target="_blank"
                 rel="noopener noreferrer"
-                style={{ color: "white", fontSize: "10px"}}
               >
                 Storyset
               </a>
               .
-            </h3></span>
-        </Col>
-      </Row>
-    </Container>
+            </h3>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
