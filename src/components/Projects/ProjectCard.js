3b1ff8e8b3dd6628fe226429f0f668361e083bc3
rgbayrak <rozgns@gmail.com>
2025-01-12 05:13:44 -0600

    replaced project card icon

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { PiCursorClickBold } from "react-icons/tb";
+import { PiCursorClickBold } from "react-icons/pi";
 import './projects.css';
 
 function ProjectCards(props) {
