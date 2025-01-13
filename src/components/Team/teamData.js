6538a0d163cc0577d204119b50a779c48137b0e9
shansitads <shansitads@gmail.com>
2025-01-13 14:22:06 -0600

    added team member links data

--- src/components/Team/teamData.js
+++ src/components/Team/teamData.js
@@ -6,42 +6,54 @@ const teamData = {
                 description: "Undergraduate Student",
                 team: "Hackathon Chair",
                 imgPath: "assets/team/Richard_Song.svg",
+                ghLink: "https://github.com/richardwsong",
+                websiteLink: "https://www.linkedin.com/in/richard-song-837015198/"
             },
             {
                 title: "Roza G. Bayrak",
                 description: "Senior Research Engineer",
                 team: "General Chair",
                 imgPath: "assets/team/Roza_Bayrak.svg",
+                ghLink: "https://github.com/rgbayrak",
+                websiteLink: "https://rgbayrak.github.io/"
             },
             {
                 title: "Catie Chang",
                 description: "Assistant Professor",
                 team: "Faculty Advisor",
                 imgPath: "assets/team/Catie_Chang.svg",
+                ghLink: "https://github.com/cately",
+                websiteLink: "https://www.cchanglab.net/"
             },
             {
                 title: "Kim Rogge-Obando",
                 description: "Graduate Student",
                 imgPath: "assets/team/Kim_Rogge-Obando.svg",
-                team: "Project Team Lead"
+                team: "Project Team Lead",
+                websiteLink: "https://www.linkedin.com/in/kim-rogge-obando/",
             },
             {
                 title: "Sarah Goodale",
                 description: "Postdoctoral Researcher",
                 imgPath: "assets/team/Sarah_Goodale.svg",
-                team: "Operations Team Lead"
+                team: "Operations Team Lead",
+                ghLink: "https://github.com/goodalse2019",
+                websiteLink: "https://www.linkedin.com/in/goodale-sarah/"
             },
             {
                 title: "Shansita D. Sharma",
                 description: "Undergraduate Student",
                 imgPath: "assets/team/Shansita_DS.svg",
-                team: "Dev Team Lead"
+                team: "Dev Team Lead",
+                ghLink: "https://github.co/shansitads",
+                websiteLink: "https://shansitads.github.io/profile"
             },
             {
                 title: "Terra Lee",
                 description: "Undergraduate Student",
                 imgPath: "assets/team/Terra_Lee.svg",
-                team: "Marketing Team Lead"
+                team: "Marketing Team Lead",
+                websiteLink: "https://www.linkedin.com/in/terra-hyunbin-lee/"
             },
             {
                 title: "Lucy Wu",
@@ -53,12 +65,16 @@ const teamData = {
                 title: "David Zaha",
                 description: "Undergraduate Student",
                 imgPath: "assets/team/David_Zaha.svg",
-                team: "Development"
+                team: "Development",
+                ghLink: "https://github.com/dgzct11",
+                websiteLink: "https://www.linkedin.com/in/davidzaha/"
             },
             {
                 title: "Sefika Ozturk",
                 description: "Undergraduate Student",
                 imgPath: "assets/team/Sefika_Ozturk.svg",
+                ghLink: "https://github.com/sefikaozturk",
+                websiteLink: "https://www.linkedin.com/in/sefikaozturk/",
                 team: "Operations"
             },
             {
@@ -77,7 +93,8 @@ const teamData = {
                 title: "Mashiur Khan",
                 description: "Undergraduate Student",
                 imgPath: "assets/team/placeholder2.svg",
-                team: "Development"
+                team: "Development",
+                ghLink: "https://github.com/mashcodes10"
             },
             {
                 title: "Crystal Jiang",
